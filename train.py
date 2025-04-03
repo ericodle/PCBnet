59f6628dc8bcc06cb26c4e7488a8eb30d157a666
eo <504068014080@tutanota.com>
2025-04-03 14:25:41 +0900

    revised training script

--- train.py
+++ train.py
@@ -2,161 +2,79 @@ from tqdm import tqdm
 from time import sleep
 from utils.dataset import CocoDataset
 from utils.model import create_model
-from utils.training_utils import SaveBestModel,train_one_epoch,val_one_epoch,get_datasets
+from utils.training_utils import SaveBestModel, train_one_epoch, val_one_epoch, get_datasets
 import torch
 import os
 import time
-from datetime import datetime
-from dataclasses import dataclass
 from simple_parsing import ArgumentParser
 from eval import evaluate_model
 from torch import nn
 from torch.utils.tensorboard import SummaryWriter
 
-def train(
-    train_dataset,
-    val_dataset,
-    epochs=2,
-    batch_size=8,
-    exp_folder="exp",
-    val_eval_freq=1,
-):
-
+def train(train_dataset, val_dataset, epochs, batch_size, exp_folder, val_eval_freq):
     date_format = "%d-%m-%Y-%H-%M-%S"
     date_string = time.strftime(date_format)
-
-    exp_folder = os.path.join("exp", "summary", date_string)
+    exp_folder = os.path.join(exp_folder, "summary", date_string)
     writer = SummaryWriter(exp_folder)
 
     def custom_collate(data):
         return data
 
-    # Dataloaders --
     train_dl = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        collate_fn=custom_collate,
-        pin_memory=True if torch.cuda.is_available() else False,
+        train_dataset, batch_size=batch_size, shuffle=True, collate_fn=custom_collate,
+        pin_memory=torch.cuda.is_available()
     )
-
     val_dl = torch.utils.data.DataLoader(
-        val_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        collate_fn=custom_collate,
-        pin_memory=True if torch.cuda.is_available() else False,
+        val_dataset, batch_size=batch_size, shuffle=False, collate_fn=custom_collate,
+        pin_memory=torch.cuda.is_available()
     )
 
-    # Device --
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = create_model(train_dataset.get_total_classes_count() + 1).to(device)
 
-    # Model --
-    faster_rcnn_model = create_model(train_dataset.get_total_classes_count() + 1)
-    faster_rcnn_model = faster_rcnn_model.to(device)
-
-    # Optimizer --
-    pg0, pg1, pg2 = [], [], []  # optimizer parameter groups
-
-    for k, v in faster_rcnn_model.named_modules():
+    pg0, pg1, pg2 = [], [], []
+    for k, v in model.named_modules():
         if hasattr(v, "bias") and isinstance(v.bias, nn.Parameter):
-            pg2.append(v.bias)  # biases
+            pg2.append(v.bias)
         if isinstance(v, nn.BatchNorm2d) or "bn" in k:
-            pg0.append(v.weight)  # no decay
+            pg0.append(v.weight)
         elif hasattr(v, "weight") and isinstance(v.weight, nn.Parameter):
-            pg1.append(v.weight)  # apply decay
+            pg1.append(v.weight)
 
-    optimizer = torch.optim.SGD(
-        pg0, lr=0.001, momentum=0.9, nesterov=True
-    ) # BN
-    
-    optimizer.add_param_group(
-        {"params": pg1, "weight_decay":  5e-4}
-    )  # add pg1 with weight_decay # Weights
-    
-    optimizer.add_param_group({"params": pg2}) # Biases
-    
+    optimizer = torch.optim.SGD(pg0, lr=0.001, momentum=0.9, nesterov=True)
+    optimizer.add_param_group({"params": pg1, "weight_decay": 5e-4})
+    optimizer.add_param_group({"params": pg2})
 
-    num_epochs = epochs
     save_best_model = SaveBestModel(output_dir=exp_folder)
 
-    for epoch in range(num_epochs):
-
-        faster_rcnn_model, optimizer, writer, epoch_loss = train_one_epoch(
-            faster_rcnn_model,
-            train_dl,
-            optimizer,
-            writer,
-            epoch + 1,
-            num_epochs,
-            device,
-        )
-        
+    for epoch in range(epochs):
+        model, optimizer, writer, epoch_loss = train_one_epoch(model, train_dl, optimizer, writer, epoch + 1, epochs, device)
         sleep(0.1)
 
-        if (epoch % val_eval_freq == 0) and epoch != 0:  
-            
+        if epoch % val_eval_freq == 0 and epoch != 0:
             sleep(0.1)
-        
         else:
-            writer, val_epoch_loss = val_one_epoch(
-                faster_rcnn_model,
-                val_dl,
-                writer,
-                epoch + 1,
-                num_epochs,
-                device,
-                log=True,
-            )
-        
+            writer, val_epoch_loss = val_one_epoch(model, val_dl, writer, epoch + 1, epochs, device, log=True)
             sleep(0.1)
-            
-            save_best_model(val_epoch_loss, 
-                            epoch, 
-                            faster_rcnn_model, 
-                            optimizer)
-            
+            save_best_model(val_epoch_loss, epoch, model, optimizer)
 
-    _, _ = val_one_epoch(
-        faster_rcnn_model, val_dl, writer, epoch + 1, num_epochs, device, log=False
-    )
-
-    writer.add_hparams(
-        {"epochs": epochs, "batch_size": batch_size},
-        {"Train/total_loss": epoch_loss, "Val/total_loss": val_epoch_loss},
-    )
-
-@dataclass
-class DatasetPaths:
-    train_image_dir: str = r"D:\Work\work\FasterRCNN-Torchvision-FineTuning\dataset\AquariumDataset\train\images"
-    val_image_dir: str = r"D:\Work\work\FasterRCNN-Torchvision-FineTuning\dataset\AquariumDataset\valid\images"
-    train_coco_json: str = r"D:\Work\work\FasterRCNN-Torchvision-FineTuning\dataset\AquariumDataset\train\_annotations.coco_neg.json"
-    val_coco_json: str = r"D:\Work\work\FasterRCNN-Torchvision-FineTuning\dataset\AquariumDataset\valid\_annotations.coco.json"
-
-@dataclass
-class TrainingConfig:
-    epochs: int = 15
-    batch_size: int = 6
-    val_eval_freq: int = 2
-    exp_folder: str = 'exp'
+    _, _ = val_one_epoch(model, val_dl, writer, epochs, epochs, device, log=False)
+    writer.add_hparams({"epochs": epochs, "batch_size": batch_size}, {"Train/total_loss": epoch_loss, "Val/total_loss": val_epoch_loss})
 
 if __name__ == "__main__":
-    
     parser = ArgumentParser()
-    parser.add_arguments(DatasetPaths,dest='dataset_config')
-    parser.add_arguments(TrainingConfig,dest='training_config')
+    parser.add_argument("--train_image_dir", type=str, required=True)
+    parser.add_argument("--val_image_dir", type=str, required=True)
+    parser.add_argument("--train_coco_json", type=str, required=True)
+    parser.add_argument("--val_coco_json", type=str, required=True)
+    parser.add_argument("--epochs", type=int, default=15)
+    parser.add_argument("--batch_size", type=int, default=6)
+    parser.add_argument("--val_eval_freq", type=int, default=2)
+    parser.add_argument("--exp_folder", type=str, default="exp")
     args = parser.parse_args()
-    
-    dataset_config: DatasetPaths = args.dataset_config
-    training_config: TrainingConfig = args.training_config
-    
-    train_ds, val_ds = get_datasets(train_image_dir=dataset_config.train_image_dir,
-                                    train_coco_json=dataset_config.train_coco_json,
-                                    val_image_dir=dataset_config.val_image_dir,
-                                    val_coco_json=dataset_config.val_coco_json)
-    train(train_ds, val_ds,
-          epochs=training_config.epochs, 
-          batch_size=training_config.batch_size,
-          val_eval_freq=training_config.val_eval_freq,
-          exp_folder=training_config.exp_folder)
-    
+
+    train_ds, val_ds = get_datasets(
+        train_image_dir=args.train_image_dir, train_coco_json=args.train_coco_json,
+        val_image_dir=args.val_image_dir, val_coco_json=args.val_coco_json
+    )
+    train(train_ds, val_ds, args.epochs, args.batch_size, args.exp_folder, args.val_eval_freq)
