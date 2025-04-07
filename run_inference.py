d9ef5ab2c04a80f0ef40f138ccae9c8398285771
eric <504068014080@tutanota.com>
2025-04-07 19:58:15 +0900

    remove comments

--- run_inference.py
+++ run_inference.py
@@ -6,7 +6,6 @@ from pycocotools.coco import COCO
 import os
 
 def main(args):
-    # Initialize dataset
     train_ds = CocoDataset(
         image_folder=args.image_folder,
         annotations_file=args.annotations_file,
@@ -14,57 +13,47 @@ def main(args):
         width=6000
     )
 
-    # Get the class names from the dataset
     classnames = train_ds.get_classnames()
     print(classnames)
 
-    # Set device
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    # Initialize model
     IF_C = InferFasterRCNN(
-        num_classes=train_ds.get_total_classes_count() + 1,  # +1 for the background class
+        num_classes=train_ds.get_total_classes_count() + 1, 
         classnames=classnames
     )
 
-    # Load model checkpoint
     IF_C.load_model(checkpoint=args.checkpoint, device=device)
 
-    # Load COCO ground truth
     cocoGt = COCO(args.annotations_file)
-    imgIds = cocoGt.getImgIds()  # all image ids
+    imgIds = cocoGt.getImgIds() 
     print(len(imgIds))
 
-    # Get image and annotations
     img_info = cocoGt.loadImgs(imgIds[args.image_id])[0]
     annIds = cocoGt.getAnnIds(imgIds=img_info['id'])
     ann_info = cocoGt.loadAnns(annIds)
     image_path = os.path.join(args.image_folder, img_info['file_name'])
 
-    # Transform image
     transform_info = CocoDataset.transform_image_for_inference(
         image_path,
         width=640,
         height=640
     )
 
-    # Perform inference
     result = IF_C.infer_image(transform_info=transform_info, visualize=False)
 
-    # Prepare ground truth and predictions
     gts_cls = [i['category_id'] for i in ann_info]
     gts_bbox = [[
         i['bbox'][0],
         i['bbox'][1],
         i['bbox'][0] + i['bbox'][2],
         i['bbox'][1] + i['bbox'][3]
-    ] for i in ann_info]  # Convert to xyxy
+    ] for i in ann_info]  
 
     pred_boxes = result['unscaled_boxes']
     pred_classes = result['pred_classes']
     pred_scores = result['scores']
 
-    # Display ground truth and predictions
     display_gt_pred(
         image_path=image_path,
         gt_boxes=gts_bbox,
@@ -72,7 +61,7 @@ def main(args):
         pred_boxes=pred_boxes,
         pred_class=pred_classes,
         pred_scores=pred_scores,
-        classnames=classnames,  # Use the dynamically obtained classnames
+        classnames=classnames,  
         box_format='xyxy'
     )
 
